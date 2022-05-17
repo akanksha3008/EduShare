e4f7a185109515315fa4917fca21d94e33e4f2b3
akanksha3008 <akankshapardeshi03@gmail.com>
2022-05-17 23:01:19 +0530

    Firebase file changed

--- EduShareBackend/Authentication/firebase.js
+++ EduShareBackend/Authentication/firebase.js
@@ -1,18 +1,18 @@
-// const firebase = require('firebase')
+// // const firebase = require('firebase')
+// import { initializeApp } from 'firebase/app';
 
-import firebase from "firebase";
-// import * as firebase from './firebase';
-var firebaseConfig = {
-    apiKey: "AIzaSyDMd-024ETAuXIWyQ2oZJ5L7tOhtTbAxQI",
-    authDomain: "filesharingedi.firebaseapp.com",
-    databaseURL: "https://filesharingedi.firebaseio.com",
-    projectId: "filesharingedi",
-    storageBucket: "filesharingedi.appspot.com",
-    messagingSenderId: "478279119993",
-    appId: "1:478279119993:web:81b3fb7564a364d697d0a8",
-    measurementId: "G-17Z81NY340"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
-exports.firebase = this.firebase;
+// // import * as firebase from './firebase';
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDMd-024ETAuXIWyQ2oZJ5L7tOhtTbAxQI",
+//     authDomain: "filesharingedi.firebaseapp.com",
+//     databaseURL: "https://filesharingedi.firebaseio.com",
+//     projectId: "filesharingedi",
+//     storageBucket: "filesharingedi.appspot.com",
+//     messagingSenderId: "478279119993",
+//     appId: "1:478279119993:web:81b3fb7564a364d697d0a8",
+//     measurementId: "G-17Z81NY340"
+// };
+// // Initialize Firebase
+// // firebase.initializeApp(firebaseConfig);
+// export const firebase = initializeApp(firebaseConfig);
+// // firebase.analytics();
